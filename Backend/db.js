a3b9759dca82767d9f0d8c38bc65332f09040863
Sakshi Kharat <sakshi.kharat@vervelo.com>
2026-03-30 10:30:26 +0530

    add ssl in db.js

--- Backend/db.js
+++ Backend/db.js
@@ -10,7 +10,10 @@ const pool = mysql.createPool({
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
