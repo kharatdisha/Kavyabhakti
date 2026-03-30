5a0e39bff736b6f2a5e13296042373b02371d6ef
Sakshi Kharat <sakshi.kharat@vervelo.com>
2026-03-30 10:33:18 +0530

    remove ssl in db.js

--- Backend/db.js
+++ Backend/db.js
@@ -11,9 +11,7 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    ssl: {
-    rejectUnauthorized: false
-  }
+   
 });
 
 module.exports = pool;
